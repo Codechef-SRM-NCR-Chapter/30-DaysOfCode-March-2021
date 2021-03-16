972782abf4ad23dd2d54447d258bba0dcd757f79
Sr1jan27 <80779925+Sr1jan27@users.noreply.github.com>
2021-03-16 23:35:47 +0530

    Update question2.py

--- answers/yourusername/Day1/question2.py
+++ answers/yourusername/Day1/question2.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+n = int(input("Enter the no. of rows"))
+k = 2*n - 2
+for i in range(0, n):
+	   
+	for j in range(0, k):
+		print(end=" ")
+	k = k - 2
+	for j in range(0, i+1):
+		print("* ", end="")
+	print("\r")

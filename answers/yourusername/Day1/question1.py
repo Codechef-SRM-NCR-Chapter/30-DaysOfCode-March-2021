30677045010f10f87aa19e2b0e09df8f52ac0eb5
Sr1jan27 <80779925+Sr1jan27@users.noreply.github.com>
2021-03-16 23:05:41 +0530

    Update question1.py

--- answers/yourusername/Day1/question1.py
+++ answers/yourusername/Day1/question1.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import math
+n = int(input("Enter the number till where the series ius to be printed = "))
+for i in range(1,n+1):
+    k = math.pow(i,3)
+    j = k + 2*i
+    print(j)

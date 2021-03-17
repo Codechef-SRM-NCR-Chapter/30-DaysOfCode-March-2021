fa511d2b081f6cef96de88e1d63a3a6d7e195ad3
Rohan Pradhan <58914647+coderrohan14@users.noreply.github.com>
2021-03-17 12:11:08 +0530

    Update question1.java

--- answers/ShariqAftab/Day1/question1.java
+++ answers/ShariqAftab/Day1/question1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class series
 {
-    public static void main()
+    public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();

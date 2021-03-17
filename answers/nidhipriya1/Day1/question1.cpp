01a6c7b1be06636aee6fc8c6c42fa37b9a6a3469
nidhipriya1 <72073584+nidhipriya1@users.noreply.github.com>
2021-03-17 22:35:53 +0530

    Create question1.cpp

---
+++ answers/nidhipriya1/Day1/question1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    for(int i = 1; i <= n; i++){
+
+        int power = pow(i, 3);
+        int ans = power + 2*i;
+        cout<<ans<<" ";
+         if(ans==n)
+    break;
+    }
+   
+
+    return 0;
+}

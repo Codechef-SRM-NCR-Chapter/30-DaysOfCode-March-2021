56b66298e0342bec49b0c8d2fa141e68142d8022
Harsh Kesarwani <77579808+hkesarwani27@users.noreply.github.com>
2021-03-17 20:32:42 +0530

    Update question1.cpp

--- answers/HarshKesarwani/Day1/question1.cpp
+++ answers/HarshKesarwani/Day1/question1.cpp
@@ -30,28 +30,7 @@ int main()
 
 
 
-//Question 1B
 
 
 
-#include <iostream>
-using namespace std;
 
-int main()
-{
-    for (int x = 1; x <= 4; x++)
-    {
-        for (int y = x; y <= 3; y++)
-        {
-            cout << "    ";
-        }
-
-        for (int z = 1; z <= x; z++)
-        {
-            cout << "*   ";
-        }
-        cout << endl;
-    }
-
-    return 0; 
-}

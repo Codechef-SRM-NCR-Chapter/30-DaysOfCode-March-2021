0b434af3666939b52be754167316b931cffa480c
Digvijay Chauhan <77578182+digvijay160@users.noreply.github.com>
2021-03-18 11:04:18 +0530

    Update question1.java

--- answers/digvijay160/Day1/question1.java
+++ answers/digvijay160/Day1/question1.java
@@ -16,23 +16,5 @@ public class Day1 {
            System.out.println(x);
         }
       
-      
-      
- //        Question_2
-        int j,k;
-      
-        for(j=1;j<=4;j++){
-          
-          for(k=(4-j);k>0;k--){
-            System.out.print(" ");
-          }
-          
-          for(k=1;k<=j;k++){
-            System.out.print("*");
-          }
-          
-        System.out.println();
-        }
-      
     }
 }

daa7cc98531c65fe57694a2fcd3b47db8f5aca48
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2021-03-18 17:31:18 +0530

    Create Question1.java

---
+++ answers/Saatvik srivastava/Day2/Question1.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Day2q1
+{
+public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number");
+    int num = sc.nextInt();
+    int temp= num;
+    int c=0,rev,s=0,d=0;
+    for(int i=1;i<=temp;i++)
+    {
+        if(num%i==0)
+        {
+            c++;
+        }
+    }
+        if(c==2)
+        {
+            while(temp>0){
+                rev=temp%10;
+                 s=rev*10+temp;
+                 temp=temp/10;
+                }
+                for(int i=1;i<=s;i++){
+    
+        if(s%i==0)
+        {
+            d++;
+        }
+    }
+        if(d==2){
+            System.out.println(num+" IS AN EMIRP NUMBER"); 
+        }
+        else{
+            System.out.println(num+" IS NOT AN EMIRP NUMBER");
+        }
+    }
+}
+}
+
+
+
+
+    
+    

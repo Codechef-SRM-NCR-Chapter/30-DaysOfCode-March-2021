71f3ba4ac56d3c3849c357b11fb3250e34e06f76
Tanish Khare <55488165+Tanish2002@users.noreply.github.com>
2021-03-18 13:15:19 +0530

    Update and rename answers/akshitparmar/day1/question1.py to answers/akshitparmar/day2/question2.py

---
+++ answers/akshitparmar/day2/question2.py
@@ -0,0 +1,18 @@
+n = int(input())
+count_digits = len(str(n)) 
+
+
+sum = 0
+x=n
+while (x!=0) : 
+
+    r = x % 10
+
+    sum = sum + int(r**count_digits)
+    count_digits = count_digits - 1
+    x = int(x/10)
+
+if sum == n : 
+    print(n," IS A DISARIUM")
+else: 
+    print(n," IS NOT A DISARIUM")

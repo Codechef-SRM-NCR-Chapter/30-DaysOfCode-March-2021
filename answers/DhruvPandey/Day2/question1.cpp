ad3299b20e15a6b419f90a8ac2d6f19dce7e5c41
Dhruv Pandey <76779907+thenextgoogler@users.noreply.github.com>
2021-03-19 02:46:54 +0530

    Update question1.cpp

--- answers/DhruvPandey/Day2/question1.cpp
+++ answers/DhruvPandey/Day2/question1.cpp
@@ -1,1 +1,33 @@
-
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+int a;
+int n;
+cout<<"Enter the number of digits  :";
+cin>>n;
+cout<<"Enter the number :";
+cin>>a;
+int sum=0;
+int i,p,d;
+int u=pow(a%10,n);
+for(i=1;i<n;i++)
+{
+int z=pow(10,i);
+int f=a/z%10;
+p=pow(f,n-i);
+sum=sum+p;
+}
+d=sum+u;
+if(a==d)
+{
+cout<<"Its a disarm number";
+}
+else
+{
+cout<<"Its not a disarm number";
+}
+return 0;
+}

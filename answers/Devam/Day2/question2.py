bc4025c063e8e444303adc4598bd826a3b3ead77
dev345 <64895552+dev345@users.noreply.github.com>
2021-03-18 12:31:57 +0530

    Create question2.py

---
+++ answers/Devam/Day2/question2.py
@@ -0,0 +1,18 @@
+def prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+def rev(num):
+    if not prime(num):
+        return False
+    reverse_num = 0
+    while num != 0:
+        d = num % 10
+        reverse_num = reverse_num * 10 + d
+        num = int(num / 10)
+    return prime(reverse_num)
+n = int(input())
+print (rev(n))

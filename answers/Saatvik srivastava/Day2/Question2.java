98de852f43f96ec0ae68a0cc3643b27e86f9f958
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2021-03-18 18:58:01 +0530

    Create Question2.java

---
+++ answers/Saatvik srivastava/Day2/Question2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+    class Question2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        String st=String.valueOf(num);
+        String str="";
+        int i,count=0;
+        for(i=st.length()-1;i>=0;i--)
+        {
+            str=str+st.charAt(i);
+        }
+        int rev= Integer.parseInt(str);
+        for(i=2;i<num;i++)
+        {
+            if(num%i==0)
+            count=1;
+        }
+        for(i=2;i<rev;i++)
+        {
+            if(rev%i==0)
+            count=1;
+        }
+        if(count!=0)
+        System.out.println(num+" is not an Emirp number");
+        else
+        System.out.println(num+" is an Emirp Number");
+    }
+}

069ae5773cec97db91ed8cb8b3eff9d8f480acb4
jshruti29 <78641804+jshruti29@users.noreply.github.com>
2021-03-19 22:43:51 +0530

    Create Q2.cpp

---
+++ answers/jshruti29/day2/Q2.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+
+int countDigits(int n) 
+{ 
+    int count_digits = 0; 
+  
+  
+    int x = n; 
+    while (x) 
+    { 
+        x = x/10; 
+  
+       
+        count_digits++; 
+    } 
+    return count_digits; 
+} 
+  
+
+bool check(int n) 
+{ 
+     
+    int count_digits = countDigits(n); 
+  
+   
+    int sum = 0; 
+    int x = n; 
+    while (x) 
+    { 
+         
+        int r = x%10; 
+  
+        
+        sum = sum + pow(r, count_digits--); 
+        x = x/10; 
+    } 
+  
+    
+  return (sum == n); 
+} 
+  
+
+int main() 
+{ 
+    int n;
+    cin>>n; 
+    if( check(n)) 
+        cout << "Disarium Number"; 
+    else
+        cout << "Not a Disarium Number"; 
+    return 0; 
+}

26d0d58ad2156d44c156a6113929a40c33c0312d
Khushi-sharma07 <khushi.sharma.22.2001@gmail.com>
2021-03-19 15:43:21 +0530

    30 Day Code Challenge

--- answers/Khushi/Day 2/Question 2.py
+++ answers/Khushi/Day 2/Question 2.py
@@ -1,18 +1,13 @@
-def isPrime(num):
-    if(num<=1):
-        return "NOT AN EMIRP NUMBER"
-    for i in range(2,num):
-        if num%i==0:
-            return "NOT AN EMIRP NUMBER"
-    return "IS AN EMIRP NUMBER"
-def solve(num):
-    if isPrime(num)=="NOT AN EMIRP NUMBER":
-        return "NOT AN EMIRP NUMBER"
-    reverse=0
-    while num!=0:
-        d=num%10
-        reverse=reverse*10+d
-        num=int(num/10)
-    return isPrime(reverse)
-n=int(input())
-print (solve(n))
+num=int(input())
+r=sum=0
+length=len(str(num))
+n=num
+while(num>0):
+    r=num%10
+    sum+=int(r**length)
+    num=num//10
+    length-=1
+if(sum==n):
+    print(str(n)+" IS A DISARIUM")
+else:
+    print(str(n)+" IS NOT A DISARIUM")

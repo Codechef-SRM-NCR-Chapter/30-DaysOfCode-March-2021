e3a6a6740c1956d80e0a26ab199bac79084b5918
hhvv970 <71218374+hhvv970@users.noreply.github.com>
2021-03-20 10:19:14 +0530

    Update Q1.cpp

--- answers/Harsh Verma/Day3/Q1.cpp
+++ answers/Harsh Verma/Day3/Q1.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 int main() {
-int n,s=0;
+int n,s=0,sum=0;
 cout<<"Enter the number of terms of the series: \n";
 cin>>n;
 for(int i=1;i<=n;i++){
     s=s*10+ i;
     cout<<s<<"+";
+    sum=sum+s;
     }
+cout<<" = "<<sum;
 return 0;
 }
-

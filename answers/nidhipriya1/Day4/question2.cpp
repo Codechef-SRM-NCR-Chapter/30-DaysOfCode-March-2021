65c42a0604208885b1df2cb899915f93a2349eec
nidhipriya1 <72073584+nidhipriya1@users.noreply.github.com>
2021-03-20 15:10:56 +0530

    Create question2.cpp

---
+++ answers/nidhipriya1/Day4/question2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int  number = 1;
+
+
+
+    for(int i = 1; i <= 4; i++)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << number << " ";
+            ++number;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}

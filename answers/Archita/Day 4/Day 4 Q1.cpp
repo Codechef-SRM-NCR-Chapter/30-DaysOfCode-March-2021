33051fac8f16f1be1c8b323f3436846656d2cd29
arc03 <80814067+arc03@users.noreply.github.com>
2021-03-20 11:19:54 +0530

    Add files via upload

---
+++ answers/Archita/Day 4/Day 4 Q1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    cout<<'\n';
+    while(n%2==0)
+    {
+        cout<<"2 ";
+        n/=2;
+    }
+    for(int i=3;i<=n;i+=2)
+    {
+        while((n%i)==0)
+        {
+             cout<<i<<" ";
+             n/=i;
+        }
+       
+    }
+    
+	return 0;
+}

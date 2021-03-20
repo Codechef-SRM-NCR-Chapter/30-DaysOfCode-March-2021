35f337d76c9860e8adcca5bb42ab83a3a125540f
Nitish Kumar <nrk1722@gmail.com>
2021-03-20 19:11:06 +0530

    Nitish1702/Day4

---
+++ answers/Nitish1702/Day4/Answer1.py
@@ -0,0 +1,16 @@
+from math import sqrt
+n= int(input())
+l=[]        
+def pfactor(n):
+    while n%2==0:
+        l.append(2)
+        n/=2
+    for i in range(3,int(sqrt(n))+1,2):
+        while n%i==0:
+            l.append(i)
+            n/=i
+    if n>2:
+        l.append(n)
+    print(l)
+pfactor(n)
+    

e50824e89b071a220b4f6b92aeaf289c44b81556
sakshi(P) <66208261+Sakshi1007@users.noreply.github.com>
2021-03-20 16:42:29 +0530

    answer added

--- answers/sakshi/Day 2/question2.py
+++ answers/sakshi/Day 2/question2.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+#day 2
+n=int(input())
+k=n
+sum=0
+l=len(str(n))
+while(n>0):
+    s=n%10
+    sum=sum+pow(s,l)
+    l-=1
+    n=int(n/10)
+
+if(k==sum):
+    print("Disparium No")
+else:
+    print("Not a Disparim No")
+    

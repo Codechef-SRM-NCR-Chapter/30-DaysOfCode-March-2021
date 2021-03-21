90ed2ca6bee5e073297c96f4c6a594904a883340
AnaAdler18 <77580335+AnaAdler18@users.noreply.github.com>
2021-03-21 15:04:48 +0530

    updating question 2 of day 2
    found a tiny mistake in my solution

--- answers/Ananya Chandra/Day 2/question2.py
+++ answers/Ananya Chandra/Day 2/question2.py
@@ -2,7 +2,9 @@ program for  Emirp number
 
 n=int(input("Enter the number\n"))
 def prime(t,i):
-    if(i<t):
+    if t==1:
+        return 0
+    elif(i<t):
         if(t%i!=0):
             return prime(t,i=i+1)
         else:
@@ -18,10 +20,14 @@ if p==1:
         r=c%10
         s=s*10+r
         c=c//10
-    p=prime(s,2)
+    if(s==n):
+        p=0
+    else:
+        p=prime(s,2)
     if p==1:
         print(n," is an Emirp number")
     else:
         print(n, " is not an Emirp number")
 else:
     print(n, " is not an Emirp number")
+

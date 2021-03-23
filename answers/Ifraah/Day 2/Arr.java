d8974e3bd05746e32fceaebdce8c258192af31d7
Ifraah <81137447+IT0506@users.noreply.github.com>
2021-03-23 10:44:06 +0530

    Day 2 questions

---
+++ answers/Ifraah/Day 2/Arr.java
@@ -0,0 +1,13 @@
+// Array initialisation
+
+class Arr{
+   public static void main(String[] args){
+        int[] arr = new int[5];
+arr[0] = 1; arr[1] = 4; arr[2] = 2; arr[3] = 5; arr[4] = 3;
+
+for(int i=0; i<arr.length; i++){
+System.out.println(arr[i]);
+}
+}
+}
+

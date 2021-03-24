cfaee23f1175588fe600da7327ee6f6063ec44c7
AnaAdler18 <77580335+AnaAdler18@users.noreply.github.com>
2021-03-24 23:08:59 +0530

    Update question1.py

--- answers/Ananya Chandra/day8/question1.py
+++ answers/Ananya Chandra/day8/question1.py
@@ -6,13 +6,16 @@ try:
 
 except:# if the input is not-integer, just continue to the next step
     unique=[]
+    repeat=[]
     s=0
     for i in arr:
         if i  not in unique:
             unique.append(i)
             s=s+i
-        else:
-            unique.remove(i)
+        elif i not in repeat :
+            repeat.append(i)
             s=s-i
+    for i in repeat:
+        unique.remove(i)
     print("The unique elements are ",unique)
-    print("The sum is : ",s)
+    print("sum: ",s)

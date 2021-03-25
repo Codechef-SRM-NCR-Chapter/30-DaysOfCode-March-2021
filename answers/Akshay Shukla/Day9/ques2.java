e2d945ca7e0e7d3700b22ebd365f794af9d150cc
AK1672 <77603181+AK1672@users.noreply.github.com>
2021-03-25 16:19:59 +0530

    Create ques2.java

---
+++ answers/Akshay Shukla/Day9/ques2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class ques2d9
+{
+    static int m=0;
+    static void SubArrays(int[]arr, int start, int end)
+    {     
+        if (end == arr.length) 
+        return;
+        else if (start > end) 
+        SubArrays(arr, 0, end + 1);
+        else
+        {
+            int sum=0;
+            for (int i = start; i < end; i++)
+            {
+                sum+=arr[i];
+            }
+            if(sum>m)
+            m=sum;
+            SubArrays(arr, start + 1, end);
+        }
+        return;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array in form [n1,n2,n3,..]");
+        String s=sc.nextLine();
+        s=s.substring(1,s.length()-1);
+        String[] arr=s.split(",");
+        int[] n=new int[arr.length];
+        for(int i=0;i<arr.length;i++)
+        n[i]=Integer.valueOf(arr[i]);
+        SubArrays(n,0,0);
+        System.out.println(m);
+    }
+}

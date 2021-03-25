7c4a23796ca8b02a3c49a9a0591c9a72b85cf85f
AkshajV1309 <onlyakshaj@gmail.com>
2021-03-25 16:16:26 +0530

    D9Q1

--- answers/AkshajV1309/Day9/D9Q1.py
+++ answers/AkshajV1309/Day9/D9Q1.py
@@ -1,6 +1,6 @@
 o=[int(x) for x in input('Enter NUMS seprated by space: ').split()]
-target = int(input('Enter the target value: '))
+t=int(input('Enter the target value: '))
 for i in range(len(o)):
     for j in range(i+1,len(o)):
-        if o[i] + o[j] == target and i!=j:
+        if o[i] + o[j]==t and i!=j:
             print([i, j])

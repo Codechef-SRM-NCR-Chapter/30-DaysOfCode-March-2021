8155afd796cef3081854b8d00d05649a37c3ccb9
rashi0711 <81012822+rashi0711@users.noreply.github.com>
2021-03-25 12:42:49 +0530

    Create question1.java

---
+++ answers/rashi0711/Day9/question1.java
@@ -0,0 +1,29 @@
+public class maxsubarray {
+
+	public static void main(String[] args) {
+		int n;int target;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();int c=0;
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		target=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((i!=j)&&(arr[i]+arr[j]==target))
+				{
+				c++;
+				
+			    if(c==1)
+				{
+					System.out.print(i+" "+j+" ");	
+				}}
+		}
+
+	}
+
+}}

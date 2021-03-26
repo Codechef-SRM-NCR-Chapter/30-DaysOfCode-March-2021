31640b5568b45ad521379a8a217edb40d9294b36
Shaurya Yamdagni <71569517+Shauryayamdagni@users.noreply.github.com>
2021-03-26 17:45:24 +0530

    Update question1.java

--- answers/ShauryaYamdagni/Day10/question1.java
+++ answers/ShauryaYamdagni/Day10/question1.java
@@ -6,21 +6,39 @@ class shaurya
     Scanner sc=new Scanner(System.in);
     System.out.println("enter the string ");
     String s=sc.nextLine();
-    s=s+"S";
-    int x,y,z,sum=0,start=0,end=0;
-    for(x=1;x<s.length();x++)
+    s=s.toUpperCase();
+    int[] alpha=new int[26];
+    int x,y,z=0;
+    char ch,ch1;
+    for(x=0;x<26;x++)
     {
-        char ch=s.charAt(x);
-        if(Character.isUpperCase(ch))
-        {
-            sum++;
-            end=x;
-            String s1=s.substring(start,end);
-        start=end;
-        System.out.println(s1);
-        }
-        
+        alpha[x]=0;
     }
-    System.out.println(sum);
+    for(x=0;x<26;x=x+1)
+    {
+        ch=(char)(x+65);
+        //System.out.println(ch);
+        for(y=0;y<s.length();y++)
+        {
+            ch1=s.charAt(y);
+            if(ch==ch1)
+            {
+            z++;
+            }
+        }
+        alpha[x]=z;
+        z=0;
+    }
+    boolean flag=true;
+    for(x=0;x<26;x++)
+    {
+        if(alpha[x]==0)
+        flag=false;
+    }
+    
+    if(flag==true)
+    System.out.println("pangram exists");
+    else
+    System.out.println("pangram doesn'texist");
     }
 }

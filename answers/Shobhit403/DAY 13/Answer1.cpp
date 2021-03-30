0cb990b45a0a5d74010f332b9b0ff9e3ff1d2e2a
Shobhit Srivastava <78103690+Shobhit403@users.noreply.github.com>
2021-03-30 11:20:07 +0530

    Answer 1

---
+++ answers/Shobhit403/DAY 13/Answer1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int sum_of_digit(int n) 
+{ 
+    if (n == 0) 
+    return 0; 
+    return (n % 10 + sum_of_digit(n / 10)); 
+} 
+int main() 
+{ 
+    int num = 12345; 
+    int result = sum_of_digit(num); 
+    cout << "Sum of digits in "<< num 
+       <<" is "<<result << endl; 
+    return 0; 
+} 

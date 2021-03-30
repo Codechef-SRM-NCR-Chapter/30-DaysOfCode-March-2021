58e7b3431996b50e214335df249d4a5ec3f2222e
AkshajV1309 <onlyakshaj@gmail.com>
2021-03-30 11:34:02 +0530

    Day 13

--- answers/AkshajV1309/Day13/D13Q2.py
+++ answers/AkshajV1309/Day13/D13Q2.py
@@ -4,5 +4,5 @@ def palindrome(L):
     elif L[0]==L[-1]:
         return palindrome(L[1:-1])
     return 0
-L=[x for x in input('Enter NUMS seprated by space: ').split()] 
-print("Palindrome") if(palindrome(L)==True) else print("Not Palindrome")
+L=[x for x in input('Enter elements seprated by space: ').split()] 
+print("Palindrome") if(palindrome(L)==1) else print("Not Palindrome")

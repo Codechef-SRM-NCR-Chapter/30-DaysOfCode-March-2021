7883ded73287d44799a4803425c02a1402829292
AkshajV1309 <onlyakshaj@gmail.com>
2021-03-31 12:03:22 +0530

    Update D14Q1.py

--- answers/AkshajV1309/Day14/D14Q1.py
+++ answers/AkshajV1309/Day14/D14Q1.py
@@ -1,8 +1,8 @@
 def remdupli(S):
     if len(S)==1:
         return S
-    if S[0] == S[1]:
+    if S[0]==S[1]:
         return remdupli(S[1:])
     return S[0]+remdupli(S[1:])
 S=input('Enter string: ')
-print(remdupli(S))
+print('Output:',remdupli(S))

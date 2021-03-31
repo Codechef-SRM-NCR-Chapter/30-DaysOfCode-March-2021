4131585be91653229337d3c8cfdbc8cd684a861f
CODERED <80800958+AnurajPariya03@users.noreply.github.com>
2021-03-31 23:51:33 +0530

    Delete question 1.py

--- answers/Anuraj Pariya/day 14/question 1.py
+++
@@ -1,8 +0,0 @@
-def remove_adjacentdup(string):
-    for i in range(len(string)-1):
-        if string[i]==string[i+1]:
-            string=string.replace(string[i]*2 , string[i])
-            return remove_adjacentdup
-        return string
-a=input('enter string')
-print(remove_adjacentdup(a))

ddd616d46e1b338cf7b84d41158f26e931bfa1d1
CODERED <80800958+AnurajPariya03@users.noreply.github.com>
2021-03-31 23:50:32 +0530

    Update question 1.py

--- answers/Anuraj Pariya/day 14/question 1.py
+++ answers/Anuraj Pariya/day 14/question 1.py
@@ -1,40 +1,8 @@
-def removeUtil(string, last_removed):
-
-	if len(string) == 0 or len(string) == 1:
-		return string
-
-	if string[0] == string[1]:
-		last_removed = ord(string[0])
-		while len(string) > 1 and string[0] ==string[1]:
-			string = string[1:]
-		string = string[1:]
-
-		return removeUtil(string, last_removed)
-
-	rem_str = removeUtil(string[1:], last_removed)
-
-	if len(rem_str) != 0 and rem_str[0] ==string[0]:
-		last_removed = ord(string[0])
-		return (rem_str[1:])
-
-	if len(rem_str) == 0 and last_removed == ord(string[0]):
-		return rem_str
-
-	return ([string[0]] + rem_str)
-
-def remove(string):
-	last_removed = 0
-	return toString(removeUtil(toList(string),last_removed))
-
-def toList(string):
-	x = []
-	for i in string:
-		x.append(i)
-	return x
-
-def toString(x):
-	return ''.join(x)
-
-STRING1 = 'AABBCC'
-print (remove(string1))
-
+def remove_adjacentdup(string):
+    for i in range(len(string)-1):
+        if string[i]==string[i+1]:
+            string=string.replace(string[i]*2 , string[i])
+            return remove_adjacentdup
+        return string
+a=input('enter string')
+print(remove_adjacentdup(a))

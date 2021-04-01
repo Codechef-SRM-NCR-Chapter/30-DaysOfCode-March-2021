84e1aab080e08096785488e22f7804b5175e14a6
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-01 13:00:14 +0530

    Update D15Q1.py

--- answers/AkshajV1309/Day15/D15Q1.py
+++ answers/AkshajV1309/Day15/D15Q1.py
@@ -1,9 +1,7 @@
 def strev(S,i=0):
-    if len(S)<=1:
-        return(S)
     if i<len(S)//2:
         S[i],S[len(S)-1-i]=S[len(S)-1-i],S[i]
         return strev(S,i+1)
     return(S)
 S=input("Enter String").split()
-print(strev(S))
+print(" ".join(strev(S)))

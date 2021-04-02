8c01f66b5a2f0463cba1dd01a73442bc8ae17ffb
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-04-02 15:38:33 +0530

    Create question2.java

---
+++ answers/ShariqAftab/Day16/question2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Number_of_ones
+{
+    public static void No_ones(int a[])
+    {
+        int index=Arrays.binarySearch(a,0);
+        System.out.println(index);
+    }
+}

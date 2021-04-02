9056991fc021e9ced67535956c5e422a38464ab6
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-02 14:33:42 +0530

    Update D16Q1.py

--- answers/AkshajV1309/Day 16/D16Q1.py
+++ answers/AkshajV1309/Day 16/D16Q1.py
@@ -1,5 +1,12 @@
+def trans(o,beg,end):
+    if end>=beg:
+        mid=beg+(end-beg)//2
+        if((mid==end or o[mid+1]==0)and(o[mid]==1)): 
+            return mid+1
+        if o[mid]==1:
+            return trans(o,(mid+1),end)
+        return trans(o,beg,mid-1)
+    else:
+        return 0
 o=[int(x) for x in input('Enter NUMS seprated by space: ').split()]
-for i in range(len(o)):
-    if o[i]==0:
-        print("No. of 1s=",i)
-        break
+print('No. 1s=',trans(o,0,len(o)-1))

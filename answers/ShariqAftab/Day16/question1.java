e5b53a3d58ab6df71ba4cf625d85cc9e054d51fb
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-04-02 15:37:52 +0530

    Create question1.java

---
+++ answers/ShariqAftab/Day16/question1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class TransitionPoint
+{
+    public static void transition(int a[])
+    {
+        int point=Arrays.binarySearch(a,1);
+        System.out.println(point);
+    }
+}

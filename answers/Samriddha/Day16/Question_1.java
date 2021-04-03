a5c6ce9b3ae6293155016a80716266ed0568e4f0
Samriddha Basu <73631825+samriddha-basu-cloud@users.noreply.github.com>
2021-04-03 23:20:56 +0530

    Update Question_1.java

--- answers/Samriddha/Day16/Question_1.java
+++ answers/Samriddha/Day16/Question_1.java
@@ -1,21 +1,32 @@
 import java.util.*;
-public class Question_1
+class Question_1
 {
+    int countOnes(int a[], int h, int l)
+    {
+        if (h >= l)
+        {
+            int m = l + (h - l) / 2;
+            if ((m == h || a[m + 1] == 0) && (a[m] == 1))
+                return m + 1;
+            if (a[m] == 1)
+                return countOnes(a, (m + 1), h);
+            return countOnes(a, l, (m - 1));
+        }
+        return 0;
+    }
     public static void main(String args[])
     {
+        Question_1 ob = new Question_1();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length of array : ");
         int n = sc.nextInt();
         int arr[]=new int[n];
         System.out.println("Enter the elements of the sorted array : ");
-        int c=0;
         for(int i =0;i<n;i++)
         {
             System.out.print("Enter element no."+(i+1)+" of array : ");
             arr[i]=sc.nextInt();
-            if (arr[i]==1)
-                c++;
         }
-        System.out.println(c);
+        System.out.println("Count of 1's in given array is " + ob.countOnes(arr, 0, n - 1));
     }
 }

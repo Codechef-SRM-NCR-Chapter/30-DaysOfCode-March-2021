acb8cc509a3791b30c89739fbca609ff6a1cdd96
Nitish Kumar <nrk1722@gmail.com>
2021-04-03 06:17:30 +0530

    Nitish1702/Day16

---
+++ answers/Nitish1702/Day16/Answer2.py
@@ -0,0 +1,14 @@
+def One(arr,l,h):
+    if h>=l:
+        mid = l +(h-l)//2
+        if ((mid == h or arr[mid+1]==abs(1-arr[0]) and (arr[mid]==abs(arr[0])))) :
+            return mid+1
+        if arr[mid]==1 :
+            return One(arr, (mid+1), h)
+        return One(arr, l, mid-1)
+     
+    return 0
+
+arr=list(map(int,input().strip('{} ').split(',')))
+print (One(arr, 0 , len(arr)-1))
+

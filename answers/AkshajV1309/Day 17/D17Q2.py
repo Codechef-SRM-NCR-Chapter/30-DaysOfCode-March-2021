769fdbdbc1a43e257f81dce0dea720d00b4000fd
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-03 17:11:16 +0530

    Update D17Q2.py

--- answers/AkshajV1309/Day 17/D17Q2.py
+++ answers/AkshajV1309/Day 17/D17Q2.py
@@ -2,11 +2,11 @@ N,o=int(input('Enter Size: ')),[]
 for i in range(N):
     o.append(int(input('Enter Element: ')))
 i,n,s=0,1,[]
-while(i<=len(o)and n<len(o)+1):
+while(i<=N and n<N+1):
     if sum(o[i:i+n])==0:
         s.append(o[i:i+n])
     i+=1
-    if(i+n==len(o)+1):
+    if(i+n==N+1):
         n+=1
         i=0
 print("Output:",len(s))

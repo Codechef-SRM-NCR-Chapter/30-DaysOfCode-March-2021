a5ccc3c5db5cd00a2bd82afb67e84222d69eb190
Nitish Kumar <nrk1722@gmail.com>
2021-04-04 22:48:58 +0530

    Nitish1702/Day18

--- answers/Nitish1702/Day18/Answer2.py
+++ answers/Nitish1702/Day18/Answer2.py
@@ -4,7 +4,6 @@ raw1={}
 for i in n:
     b='{0:b}'.format(i)
     c.append(b)
-print(c)
 for j in c:
     count=j.count('1')
     if count in raw1:
@@ -12,7 +11,6 @@ for j in c:
     else:
         raw1[count]=[int(j,2)]
 sraw1=sorted(raw1.items())
-print(sraw1)
 for k in range(len(sraw1)-1,-1,-1) :
     print(str(sraw1[k][1]).strip('[]').replace(',',' '),end=' ')
 

7cc0cbea28b02d974a9b2d500e76cfa0e451187f
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-04 13:01:42 +0530

    Day 18

---
+++ answers/AkshajV1309/Day 18/D18Q2.py
@@ -0,0 +1,9 @@
+o=[int(x) for x in input('Enter NUMS seprated by space: ').split()]
+n,d=[],[]
+for i in range(len(o)):
+    c=list(bin(o[i])).count('1')
+    d=[o[i],c]    
+    n.append(d)
+    n.sort(key=lambda d: (d[1]), reverse=True)
+n=[d[0] for d in n]
+print(n)

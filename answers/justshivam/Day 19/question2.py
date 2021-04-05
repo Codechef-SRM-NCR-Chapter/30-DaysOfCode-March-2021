addd5c6be45dd3a01232a7c7f7bcf663583002aa
Shivam Singh <59390818+justshivam@users.noreply.github.com>
2021-04-05 15:09:51 +0530

    Update question2.py

--- answers/justshivam/Day 19/question2.py
+++ answers/justshivam/Day 19/question2.py
@@ -4,6 +4,6 @@ count = 0
 for i in range(0, n-2):
     for j in range(i+1, n-1):
         for k in range(j+1, n):
-            if arr[i] + arr[j] + arr[k] == x:
+            if arr[i] + arr[j] + arr[k] < x:
                 count += 1
 print(x)

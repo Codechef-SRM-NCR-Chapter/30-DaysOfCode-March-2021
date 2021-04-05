866283ef33b49b46757394f93b27a98b9787a837
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-05 12:04:21 +0530

    Day 19

---
+++ answers/AkshajV1309/Day 19/D19Q2.py
@@ -0,0 +1,12 @@
+N,X=int(input('Enter Size of array: ')),int(input('Enter Sum: '))
+o,c=[int(x) for x in input('Enter NUMS seprated by space: ').split()][:N],0
+for i in range( 0 ,N-2):
+    for j in range( i+1 ,N-1):
+        for k in range( j+1, N):
+            if (o[i] + o[j] + o[k] < X):
+                c+=1
+print('Output: ',c)
+    
+
+  
+

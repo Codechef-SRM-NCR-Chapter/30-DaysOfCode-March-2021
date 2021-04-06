f29fa170edba0cad737003c853f030542d24468b
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-06 17:58:56 +0530

    Update D20Q1.py

--- answers/AkshajV1309/Day 20/D20Q1.py
+++ answers/AkshajV1309/Day 20/D20Q1.py
@@ -1,6 +1,6 @@
-T=int(input('Enter test cases'))
+T,F=int(input('Enter test cases: ')),[]
 while(T>0):
-    N=list(input('Enter Number'))
+    N=list(input('Enter Number: '))
     S=[]
     for i in range(len(N)):
         if i>0 and int(N[i])==int(N[i-1]):
@@ -8,5 +8,7 @@ while(T>0):
         else:
             S.append(N[i])
             i+=1
-    print("Output: ","".join(S))
+    F.append("".join(S))
     T-=1
+for i in F:
+    print('Output:',i)

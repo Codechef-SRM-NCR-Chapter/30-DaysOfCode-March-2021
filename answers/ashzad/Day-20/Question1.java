50e6f9526cb172755670c8bb175868cba2d05009
ashzad123 <78037204+ashzad123@users.noreply.github.com>
2021-04-06 23:56:45 +0530

    Create Question1.java

---
+++ answers/ashzad/Day-20/Question1.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class BBrackets {
+   
+    public static char pair(char c) {
+        if(c=='}') {
+            return '{';
+        }
+        else if(c==')') {
+            return '(';
+        }
+        else {
+            return '[';
+        }
+    }
+   
+    public static boolean checkBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<str.length();i++) {
+            char c = str.charAt(i);
+            if( c=='{' || c=='(' || c=='[' ) {
+                stack.push(c);
+            }
+            else if( c=='}' || c==')' || c==']' ) {
+               
+                if(stack.isEmpty()) {
+                    return false;
+                }
+               
+                if(  pair(c) ==stack.peek() ) {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+               
+            }
+           
+           
+        }
+        if(stack.isEmpty()) {
+            return true;
+        }
+        return false;
+
+    }
+   
+   
+        public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+       
+        String string = sc.nextLine();
+        System.out.println(
+                checkBalanced(string)
+                );
+    }
+
+}

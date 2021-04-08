bbbdf846f8f17474b4d0629e438b1ac40693b97f
AnaAdler18 <77580335+AnaAdler18@users.noreply.github.com>
2021-04-08 11:38:36 +0530

    Create question2.py

---
+++ answers/Ananya Chandra/day22/question2.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the size of queue\n"))
+K=int(input("Enter the no. of elements to be reversed\n"))
+a=[]
+ans=[]
+print("Enter the integer elements")
+for i in range(n):
+    a.append(int(input()))
+i=0
+while i<K//2:
+    t=a[i]
+    a[i]=a[K-1-i]
+    a[K-1-i]=t
+    i+=1
+print(a)
+

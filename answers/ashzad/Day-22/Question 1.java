549b1ae674da52349dad311d30de011d33c36003
ashzad123 <78037204+ashzad123@users.noreply.github.com>
2021-04-08 14:45:44 +0530

    Create Question 1.java

---
+++ answers/ashzad/Day-22/Question 1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class StringD22
+{
+    public static void main(String args[]) throws Exception 
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String arr[] = new String[n];
+        s.nextLine();
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextLine();
+            // System.out.println(arr[i]);
+
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(removeEvenSubString(arr[i]));
+        }
+    }
+
+    static String removeEvenSubString(String s) 
+    {
+        Stack<Character> stc = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (stc.isEmpty()) {
+                stc.push(s.charAt(i));
+            } else if (stc.peek().equals(s.charAt(i))) {
+                stc.pop();
+            } else {
+                stc.push(s.charAt(i));
+            }
+
+        }
+        String out = "";
+        if (stc.isEmpty()) {
+            out="-1";
+        } else {
+            while (!stc.isEmpty()) {
+                out = stc.pop() + out;
+            }
+        }
+        return out;
+
+    }
+}

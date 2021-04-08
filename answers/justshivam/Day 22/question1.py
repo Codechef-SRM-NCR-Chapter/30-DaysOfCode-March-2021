eb3cd22ea2216107564bff96385c171d9ae853af
Shivam Singh <stdnt.shivam@gmail.com>
2021-04-08 19:01:26 +0530

    Day 22

---
+++ answers/justshivam/Day 22/question1.py
@@ -0,0 +1,10 @@
+def run(string):
+    for i in range(1, len(string)):
+        if string[i] == string[i-1]:
+            arr = [ch for ch in string]
+            del arr[i-1:i+1]
+            return run(''.join(arr))
+    return string
+
+
+print(run('101001'))

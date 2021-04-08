34ac120475bd62c8528cc81dd1887eeff86352c0
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-08 15:11:53 +0530

    Day 22

---
+++ answers/AkshajV1309/Day22/D22Q1.py
@@ -0,0 +1,13 @@
+S,T=input("Enter Binary Str: "),[]
+for i in range(len(S)-1,-1,-1):
+    if T==[]:
+        T.append(S[i])
+    else:
+        c=T.pop()
+        if S[i]!=c:
+            T.extend([c,S[i]])
+    print(T)
+print(-1) if len(T)==0 else print("".join(T[::-1]))
+
+
+

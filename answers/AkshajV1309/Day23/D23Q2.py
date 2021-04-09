36f64b18b971e9ecab7fd2ecaf0dec1845e2b03d
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-09 14:59:59 +0530

    Day 23

---
+++ answers/AkshajV1309/Day23/D23Q2.py
@@ -0,0 +1,30 @@
+from collections import deque
+T,F=int(input('Enter Test Cases')),[]
+while(T>0):
+    N=int(input('Enter N'))
+    if N in [1,2]:
+        F.append(N)
+        T-=1
+        continue
+    Q=deque()
+    Q.extend(['1','2'])
+    C=2
+    while(C<N):
+        Q.append(Q[0]+'1')
+        C+=1
+        if(C==N):
+            break
+        else:
+            Q.append(Q[0]+'2')
+            Q.popleft()
+            C+=1
+    F.append(Q[-1])
+    T-=1
+for C in F:
+ print(C)
+
+ 
+
+        
+        
+    

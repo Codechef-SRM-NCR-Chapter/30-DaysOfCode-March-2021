c15a16b04e1b5128e8addd349dd65f86ebfddb7e
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-04-09 23:04:16 +0530

    Create question2.java

---
+++ answers/ShariqAftab/Day22/question2.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+public class ReverseQUEUE
+{
+    static Queue<Integer> queue;
+    static void reverse(int k)
+    {
+        if (queue.isEmpty() == true
+            || k > queue.size())
+            return;
+        if (k <= 0)
+            return;
+ 
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < k; i++)
+        {
+            stack.push(queue.peek());
+            queue.remove();
+        }
+        while (!stack.empty()) {
+            queue.add(stack.peek());
+            stack.pop();
+        }
+        for (int i = 0; i < queue.size() - k; i++) {
+            queue.add(queue.peek());
+            queue.remove();
+        }
+    }
+    static void Print()
+    {
+        while (!queue.isEmpty()) {
+            System.out.print(queue.peek() + " ");
+            queue.remove();
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        queue = new LinkedList<Integer>();
+        System.out.println("Enter the size of the queue:");
+        int n=sc.nextInt();
+        System.out.println("Enter the values in the Queue:");
+        for(int i=0;i<n;i++)
+        {
+            int s=sc.nextInt();
+            queue.add(s);
+        }
+        System.out.println("Enter value of k:");
+        int k = sc.nextInt();
+        System.out.println("Output:");
+        reverse(k);
+        Print();
+    }
+}

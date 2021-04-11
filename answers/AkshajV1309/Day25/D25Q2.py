10aef41268c6c723e4bea8b92d0779925e4dbe93
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-11 16:42:58 +0530

    Day25

---
+++ answers/AkshajV1309/Day25/D25Q2.py
@@ -0,0 +1,5 @@
+T=int(input('Enter Test cases:'))
+while(T>0):
+    N=[int(x) for x in input('Enter X & Y:').split()]
+    print("Can't reach destination")if N[1]>N[0]or 0>N[0]or 0>N[1] else print('Output:',N[0])
+    T-=1

89a246e2e857cba0ebd5e32490d83acdd2bf3c2e
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-12 11:14:36 +0530

    Day 26

---
+++ answers/AkshajV1309/Day26/D26Q2.py
@@ -0,0 +1,7 @@
+N=int(input('Enter Size: '))
+C,k=[int(x) for x in input('Enter lower and upper limits: ').split()][:N],0
+for i in range(1,len(C)):
+    if C[i-1]>C[i]:
+        k+=C[i-1]-C[i]
+        C[i]+=(C[i-1]-C[i])
+print('Output:',k)

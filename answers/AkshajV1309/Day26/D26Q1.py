852adae5bb3f8cdad244de0f407f3c7e2c43e335
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-12 11:02:19 +0530

    Update D26Q1.py

--- answers/AkshajV1309/Day26/D26Q1.py
+++ answers/AkshajV1309/Day26/D26Q1.py
@@ -5,7 +5,7 @@ def isprime(i):
     return 1
 T,F=int(input('Enter Test cases: ')),[]
 while(T>0):
-    I,P,C=[int(x) for x in input('Enter lower and upper limit: ').split()],[],[]
+    I,P,C=[int(x) for x in input('Enter lower and upper limits: ').split()],[],[]
     for i in range(I[0],I[1]+1):
         P.append(i) if isprime(i)==1 else C.append(i)
     F.append(len(P)*len(C))

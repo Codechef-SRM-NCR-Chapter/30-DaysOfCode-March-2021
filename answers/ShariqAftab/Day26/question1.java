6c1a04be69a275e5be0588e44e47f33ad483a50b
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-04-14 00:07:03 +0530

    Create question1.java

---
+++ answers/ShariqAftab/Day26/question1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Trying
+{
+    public static boolean isPrime(int n)
+    {
+        int i,c=0;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+                c++;
+        }
+        if(c==2)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> a1=new ArrayList<>();
+        ArrayList<Integer> a2=new ArrayList<>();
+        System.out.println("Enter the number of test cases:");
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            System.out.println("Enter the range:");
+            int ll=sc.nextInt();
+            int ul=sc.nextInt();
+            for(int i=ll;i<=ul;i++)
+            {
+                if(isPrime(i)==true)
+                    a1.add(i);
+                else
+                    a2.add(i);
+            }
+            int p=a1.size() * a2.size();
+            System.out.println("Output : "+p);
+        }
+    }
+}

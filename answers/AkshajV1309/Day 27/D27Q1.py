d3019ae7ec82eb22703acb8d35b58da8ed95fd00
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-13 14:30:06 +0530

    Update D27Q1.py

--- answers/AkshajV1309/Day 27/D27Q1.py
+++ answers/AkshajV1309/Day 27/D27Q1.py
@@ -1,8 +1,8 @@
 P=[int(x) for x in input('Enter Length,Rotations,Queries: ').split()]
 I,p=[int(x) for x in input('Enter Array: ').split()][:P[0]],[]
 for i in range(P[1]):
-    I.insert(0, I.pop())
+    I.insert(0,I.pop())
 for i in range(P[2]):
     p.append(I[int(input('Enter Query: '))])
 for i in p:
-    print(i)
+    print('Output:',i)

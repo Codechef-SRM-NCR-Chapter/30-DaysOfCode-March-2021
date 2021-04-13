e28b10e5aecba92253314ce0ebff5b257d32a288
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-04-13 23:55:00 +0530

    Create question2.java

---
+++ answers/ShariqAftab/Day24/question2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Arrays
+{
+    
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements in the  array :");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Output");
+        System.out.println(Minstep(arr));
+    }
+  static int Minstep(int inputArray [])
+    {
+        int Maxvalue = inputArray[0];
+
+        for(int i = 1; i < inputArray.length; i++)
+        {
+            if(inputArray[i] > Maxvalue)
+            {
+                Maxvalue = inputArray[i];
+            }
+        }
+        return Maxvalue;
+    }
+}
+

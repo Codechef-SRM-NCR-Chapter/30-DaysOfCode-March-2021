c19a1939eb5440f0982edcc4357988da7cca1053
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-14 13:40:19 +0530

    Day 28

---
+++ answers/AkshajV1309/Day 28/D28Q2.py
@@ -0,0 +1,18 @@
+N,R=int(input('Enter No. Test-Cases: ')),[]
+while(N>0):
+    I=[int(x) for x in input('Enter Length,Req. no. peaks: ').split()]
+    if I[1]<I[0]/2:
+        O,c=[str(x) for x in range(1,I[0]+1)],0
+        if I[1]==0:
+            R.append("".join(O))
+        else:
+            for i in range(I[0]-1,1,-2):
+                O[i],O[i-1]=O[i-1],O[i]
+                c+=1
+                if c==I[1]:
+                    break
+            R.append("".join(O))  
+    else:
+        R.append(-1)
+    N-=1
+print(R)

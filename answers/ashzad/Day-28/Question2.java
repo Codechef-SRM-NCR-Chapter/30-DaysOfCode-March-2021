05192bd149aff4c2204445725fc97483e0c4a518
Ashzad Kamal <78037204+ashzad123@users.noreply.github.com>
2021-04-14 11:42:08 +0530

    Create Question2.java

---
+++ answers/ashzad/Day-28/Question2.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class ArrayPeakvalue
+{
+    public static void main()
+    {
+        Scanner sc=  new Scanner (System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n,k;
+            n=sc.nextInt();
+            k=sc.nextInt();
+            if(n%2!=0)
+            {
+                if(k>n/2)
+                {
+                    System.out.println(-1);
+                }
+                else
+                {
+                    int c=0;
+                    int i=1;
+                    int ls[]=new int[n+1];
+                    for(int x=1;x<=n;x++)
+                    {
+                        ls[x]=x;
+                    }
+                    while(c<k)
+                    {
+                        int a=ls[i];
+                        ls[i]=ls[i+1];
+                        ls[i+1]=a;                     
+                        c++;
+                        i+=2;
+                    }
+                    for(int x=1;x<=n;x++)
+                    System.out.print(ls[x]+" ");
+                }
+            }
+            else
+            {
+                 if(k>((n/2) -1))
+                {
+                    System.out.println(-1);
+                }
+                else
+                {
+                    int c=0;
+                    int i=1;
+                    int ls[]=new int[n+1];
+                    for(int x=1;x<=n;x++)
+                    {
+                        ls[x]=x;
+                    }
+                    while(c<k)
+                    {
+                        int a=ls[i];
+                        ls[i]=ls[i+1];
+                        ls[i+1]=a;                     
+                        c++;
+                        i+=2;
+                    }
+                    for(int x=1;x<=n;x++)
+                    System.out.print(ls[x]+" ");
+                }
+            }
+        }
+    }
+}

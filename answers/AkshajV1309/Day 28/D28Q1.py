d12db95aa783a643f05be4080b7151366e7ca67a
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-14 15:28:17 +0530

    Day 28

---
+++ answers/AkshajV1309/Day 28/D28Q1.py
@@ -0,0 +1,16 @@
+import math
+N=int(input('Enter Size: '))
+O=[int(x) for x in range(1,N+1)]
+if sum(O)%2!=0:
+    print('No')
+else:
+    print('Yes')
+    t=N//2
+    s=math.ceil(t/2)
+    A=O[:t-s]+O[-s:]
+    print(len(A))
+    print(A)
+    print(N-len(A))
+    print(O[t-s:-s])
+
+    

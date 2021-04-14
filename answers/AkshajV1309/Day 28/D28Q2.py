f8f0c7a81fab0cbfd3be4eaf925c22237f0f0aa6
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-14 18:05:37 +0530

    Update D28Q2.py

--- answers/AkshajV1309/Day 28/D28Q2.py
+++ answers/AkshajV1309/Day 28/D28Q2.py
@@ -3,15 +3,13 @@ while(N>0):
     I=[int(x) for x in input('Enter Length,Req. no. peaks: ').split()]
     if I[1]<I[0]/2:
         O,c=[str(x) for x in range(1,I[0]+1)],0
-        if I[1]==0:
-            R.append("".join(O))
-        else:
-            for i in range(I[0]-1,1,-2):
+        for i in range(I[0]-1,1,-2):
+            if c==I[1]:
+                break
+            else:
                 O[i],O[i-1]=O[i-1],O[i]
                 c+=1
-                if c==I[1]:
-                    break
-            R.append("".join(O))  
+        R.append("".join(O))  
     else:
         R.append(-1)
     N-=1

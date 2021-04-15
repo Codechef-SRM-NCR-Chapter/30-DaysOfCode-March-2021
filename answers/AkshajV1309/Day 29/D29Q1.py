9e7bab8d868eace885bedcc276c7dd7b290ae972
AkshajV1309 <onlyakshaj@gmail.com>
2021-04-15 15:38:59 +0530

    Update D29Q1.py

--- answers/AkshajV1309/Day 29/D29Q1.py
+++ answers/AkshajV1309/Day 29/D29Q1.py
@@ -1,8 +1,8 @@
 T,R=int(input('Enter no. Test Cases: ')),[]
 while(T>0):
     P=[int(x) for x in input('Enter No. Cases,Sum: ').split()]
-    A=sorted([int(x) for x in input('Enter Numbers: ').split()][:P[0]])
-    B=sorted([int(x) for x in input('Enter Numbers: ').split()][:P[0]])
+    A=sorted([int(x) for x in input('Enter Numbers(A): ').split()][:P[0]])
+    B=sorted([int(x) for x in input('Enter Numbers(B): ').split()][:P[0]])
     for i in range(P[0]):
         if A[i]+B[-(i+1)]>P[1]:
             R.append('No')
@@ -11,4 +11,4 @@ while(T>0):
             R.append('Yes')
     T-=1
 for T in R:
-    print(T)
+    print('Output:',T)

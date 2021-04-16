2dcdf9c8d2998c32bd629cd5cf93696918936ca3
sequel-tj <74481733+sequel-tj@users.noreply.github.com>
2021-04-17 00:46:17 +0530

    Create question1.py

---
+++ answers/Tanmay Jaiswal/Day30/question1.py
@@ -0,0 +1,27 @@
+import sys
+
+print("Input:",end="")
+n,m = list(map(int,input("\t").split()))
+
+a = []
+b = []
+
+for i in range(m):
+    a.append(input("\t").split())
+    if (len(a[i][0])) <= (len(a[i][1])):
+        b.append(0)
+    else:
+        b.append(1)
+        
+x = input("\t").split()
+
+if len(x) != n:
+    print("This line should contain str(n) words, try again.")
+    sys.exit()
+    
+print("\nOutput:\t",end="")
+
+for i in x:
+    for j in range(m):
+        if i in a[j]:
+            print(a[j][c[j]],end=" ")

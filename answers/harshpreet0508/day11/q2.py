b02c51b0d02977c0c8fc22e5b0cc179f258bdb49
Rohan Pradhan <58914647+coderrohan14@users.noreply.github.com>
2021-04-17 14:29:03 +0530

    Revert "merge day11"

--- answers/harshpreet0508/day11/q2.py
+++
@@ -1,7 +0,0 @@
-# Given string and patterns, find the number of times pattern appears in string
-
-s = input('Enter a string ')
-n = int(input("Enter no of patterns: "))
-for i in range(0,n):
-    p = input()
-    print(s.count(p))

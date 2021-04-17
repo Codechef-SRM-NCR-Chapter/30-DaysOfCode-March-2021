a76a1340f69934b012c2dbc8820f92d06c81db48
Rohan Pradhan <58914647+coderrohan14@users.noreply.github.com>
2021-04-17 14:31:10 +0530

    Revert "merge day13"

--- answers/harshpreet0508/day12/q2.cpp
+++ answers/harshpreet0508/day12/q2.cpp
@@ -1,30 +1,51 @@
-// Given a string s, count the number of substrings that start and end with 1
-
-#include<iostream>
-using namespace std; 
+// Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 
+// 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
   
-int count(string s) 
-{ 
-    int c = 0;
-
-    for (int i=0; s[i] !='\0'; i++) 
-    { 
-        if (s[i] == '1') 
-        { 
-            for (int j=i+1; s[j] !='\0'; j++) 
-            {
-                if (s[j] == '1') 
-                    c++; 
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int res = 0;
+        int z = 0;
+        int o = 0;
+        bool check;
+        if(s[0]=='0'){
+            z++;
+            check = true;
+        }
+        else{
+            o++;
+            check = false;
+        }
+        for(int i=1;i<s.size();i++){
+            if(check){
+                if(s[i]=='0'){
+                    z++;
+                    if(o>=z){
+                        res++;
+                    }
+                }
+                else{
+                    res++;
+                    o=0;
+                    check = false;
+                    o++;
+                }
             }
-        } 
-    } 
-    return c; 
-} 
-  
-int main() 
-{ 
-string s;
-cin>>s;
-cout << count(s); 
-return 0; 
-} 
+            else{
+                if(s[i]=='1'){
+                    o++;
+                    if(z>=o){
+                        res++;
+                    }
+                }
+                else{
+                    res++;
+                    z=0;
+                    check = true;
+                    z++;
+                }
+            }
+        }
+        return res;
+    }
+};

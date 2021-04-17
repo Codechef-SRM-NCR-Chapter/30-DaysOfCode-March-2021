2fb575d06a3c86bc890e7fb97d321eba8f93157f
CODERED <80800958+AnurajPariya03@users.noreply.github.com>
2021-04-17 07:07:04 +0530

    Create question 1.py

---
+++ answers/Anuraj Pariya/day 30/question 1.py
@@ -0,0 +1,32 @@
+#last day of coding 
+inp = input("Enter n , m: ")
+n , m = inp.split()
+n = int(n)
+m = int(m)
+
+lan1 = []
+lan2 = []
+
+for i in range(0 , m):
+    k = input("Enter the words: ")
+    z = k.split()
+    lan1.append(z[0])
+    lan2.append(z[1])
+
+sent = [None]*n
+sen = input("Enter the sentence: ")
+s = sen.split()
+
+for i in range(0 , n):
+    sent[i] = s[i]
+
+for i in range(0 , len(sent)):
+    for j in range(0 , len(lan1)):
+        if sent[i] == lan1[j]:
+            if len(lan1[j]) > len(lan2[j]):
+                sent[i] = lan2[j]
+        if sent[i] == lan2[j]:
+            if len(lan1[j]) == len(lan2[j]):
+                sent[i] = lan1[j]    
+
+print(" ".join(sent))

e3114d9d950114487ab5d7f2fe4bdcc176f4f6c5
Ifraah <81137447+IT0506@users.noreply.github.com>
2021-05-08 10:35:04 +0530

    Update Question 1.java

--- answers/Ifraah/Day 1/Question 1.java
+++ answers/Ifraah/Day 1/Question 1.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 class Sequence
-
+{
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a no. :");
         int n = scan.nextInt();
 
         int i, j;
 
         for(i=1; i<=n; i++){
               j = i * (i*i+2);
-             System.out.println(j);
+             System.out.print(" "+j);
            }
      }
 }
